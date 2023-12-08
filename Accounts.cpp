b3199b5cb453f131f9db88302d32bb87b391e796
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-12-08 10:10:29 -0500

    Accounts.cpp

---
+++ Accounts.cpp
@@ -0,0 +1,37 @@
+/**********************************************
+ * Accounts.cpp
+ * Created by Ryan Aljaari
+ **********************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "Accounts.h"
+
+/**********************************************
+ * Constructor
+ **********************************************/
+account::account(string argf, string argl, int phoneNum, string email, string addressName, float money)
+{ 
+  first = argf;
+  last = argl;
+  phone = phoneNum;
+  mail = email;
+  address = addressName;
+  next = NULL;
+  balance = money;
+  id = -1;
+  // increment account_lists based on how many times the constructor is called upon
+}
+
+/**********************************************
+ * Destructor
+ **********************************************/
+account::~account()
+{ 
+  if(next!=NULL) delete next;
+}
+
+
