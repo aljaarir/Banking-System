ca6e112c2f4b1c7ac29cf273b1852c6c9935fa83
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-12-08 10:13:41 -0500

    Accounts.h

--- Accounts.h
+++ Accounts.h
@@ -8,7 +8,7 @@ class account
     friend class bank;
   
     public:
-        account(string argf = "", string argl = "", int phone = 0, string email = "", string address = "", float balance = 0.0F);  // Default constructor -- Creates account
+        account(string argf = "", string argl = "", int long phone = 0, string email = "", string address = "", float balance = 0.0F);  // Default constructor -- Creates account
         ~account();                                         // Destructor 
 
 
